f4081c342f0faf8925e65670a31692a62e22731a
AyumuTazawa <tazawaayumu@gmail.com>
2020-09-25 17:28:31 +0900

    Initial Commit

---
+++ Sonaeru/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sonaeru
+//
+//  Created by 田澤歩 on 2020/09/25.
+//  Copyright © 2020 net.ayumutazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
